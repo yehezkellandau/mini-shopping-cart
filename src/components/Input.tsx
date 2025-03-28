69d1994f6128245ef2626549e6771fad3101514a
yeheland <yeland_97@yhaoo.fr>
2025-03-28 09:45:10 -0400

    component Input creation

---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+interface InputProps {
+    label: string;
+    type: string;
+    value: string | number;
+}
+
+const Input = (props: InputProps) => {
+    const {
+        label,
+        type="text",
+        value,
+    } = props;
+    return (
+    <div>
+        <label>
+            {label} 
+            <input
+            type={type}
+            value={value}
+            ></input>
+        </label>
+    </div>
+    );
+};
+export default Input;
